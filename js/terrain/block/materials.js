613afd2b567fd3961682a3dd29846eed6cb87c15
alx2007 <98368326+alx2007@users.noreply.github.com>
2022-05-01 21:09:42 +0100

    Create materials.js

---
+++ js/terrain/block/materials.js
@@ -0,0 +1,12 @@
+const grass = "./textures/grassBlock/grassBlock";
+
+export const materials = {
+  grassBlock : {
+    front : `${grass}Side.png`,
+    back : `${grass}Side.png`,
+    top : `${grass}Top.jpg`,
+    bottom : `${grass}Bottom.jpg`,
+    left : `${grass}Side.png`,
+    right : `${grass}Side.png`
+  }
+};
